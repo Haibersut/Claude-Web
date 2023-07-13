e4fef9028c3c9c8c757c2387e162fde0c581d430
Matt Gideon <117586514+Haibersut@users.noreply.github.com>
2023-07-13 20:48:43 +0800

    alpha版本
    尝试加入了附件功能，但是无法正常获得返回值。
    
    加入了创建会话保持功能。

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import aiohttp
 import asyncio
 import json
 import uuid
+import mimetypes
 from loguru import logger
 
 
@@ -11,6 +12,7 @@ class ClaudeWeb:
         self.proxy: str = proxy
         self.url: str = url or "https://claude.ai"
         self.timeout: int = timeout
+        self.sessions = {}
 
     async def ask_with_stream(self, organization_uuid, conversation_uuid, message):
         """向 Claude AI 发送消息"""
@@ -46,6 +48,7 @@ class ClaudeWeb:
         request_url = f'{self.url}/api/organizations/{organization_uuid}/chat_conversations'
         headers = self._get_headers()
         session_uuid = str(uuid.uuid4())
+        self.sessions[len(self.sessions) + 1] = session_uuid
         payload = {"uuid": session_uuid, "name": name}
         try:
             async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=self.timeout)) as session:
@@ -72,6 +75,34 @@ class ClaudeWeb:
             logger.error(f"An error occurred in get_conversation: {e}")
             raise
 
+    async def convert_file(self, file_content, file_name):
+        """文件格式化，还用不了（"""
+
+        request_url = f'{self.url}/api/convert_document'
+        headers = {
+            "Cookie": self.session_token,
+            "Accept": "text/event-stream",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                          "Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1823.79",
+        }
+
+        mime_type = mimetypes.guess_type(file_name)[0]
+
+        if mime_type not in ['application/pdf', 'text/csv', 'text/plain']:
+            raise ValueError(
+                f"不支持的文件类型: {mime_type}. 只支持pdf, csv和txt文件")
+        data = aiohttp.FormData()
+        data.add_field('file', file_content, filename=file_name, content_type=mime_type)
+
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.post(request_url, headers=headers, data=data, proxy=self.proxy) as response:
+                    print(response)
+
+        except Exception as e:
+            logger.error(f"An error occurred in convert_file: {e}")
+            raise
+
     async def get_session(self, organization_uuid):
         """获得用户的所有会话"""
         request_url = f'{self.url}/api/organizations/{organization_uuid}/chat_conversations'
@@ -88,7 +119,7 @@ class ClaudeWeb:
             raise
 
     async def get_uuid(self):
-        """获得用户的uuid"""
+        """获得用户的 UUID"""
         request_url = f'{self.url}/api/organizations'
         headers = self._get_headers()
         try:
@@ -101,6 +132,10 @@ class ClaudeWeb:
         except Exception as e:
             logger.error(f"An error occurred in get_uuid: {e}")
             raise
+
+    def get_session_by_id(self, id):
+        """根据编号获取会话的 UUID"""
+        return self.sessions.get(id)
 
     def _get_headers(self):
         return {"Cookie": self.session_token,
@@ -127,26 +162,30 @@ class ClaudeWeb:
         """测试函数"""
 
         # 获得用户的uuid
-        organization_uuid = await self.get_uuid()
+        # organization_uuid = await self.get_uuid()
 
         # 获得用户的所有会话
         # await self.get_session(organization_uuid)
 
         # 创建一个会话
-        conversation_uuid = await self.create_session(organization_uuid)
+        # conversation_uuid = await self.create_session(organization_uuid)
 
         # 获得某个会话的所有消息
         # await self.get_conversation(organization_uuid, conversation_uuid)
 
         # 向Claude AI发送消息
-        message = "hello"
-        async for completion in self.ask_with_stream(organization_uuid, conversation_uuid, message):
-            logger.debug(completion)
+        # message = "hello"
+        # async for completion in self.ask_with_stream(organization_uuid, conversation_uuid, message):
+            # logger.debug(completion)
+
+        # 文件转换，有bug
+        # with open("test.pdf", "rb") as file_content:
+            # await self.convert_file(file_content, "test.pdf")
 
 
 if __name__ == "__main__":
     """使用例子"""
-    session_token = "xxxxxx"  # 您的会话令牌
+    session_token = "xxxxx"  # 您的会话令牌
     proxy = None  # 您的代理
     claude = ClaudeWeb(None, session_token, proxy)
     asyncio.run(claude.main())
