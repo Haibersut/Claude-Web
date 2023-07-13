a1e70853d656e73a7956ad0e35ab7fe6332a7095
Matt Gideon <117586514+Haibersut@users.noreply.github.com>
2023-07-13 18:22:07 +0800

    加入超时处理

--- main.py
+++ main.py
@@ -6,14 +6,11 @@ from loguru import logger
 
 
 class ClaudeWeb:
-    def __init__(self, url, session_token, proxy):
+    def __init__(self, url, session_token, proxy, timeout=60):
         self.session_token: str = session_token
         self.proxy: str = proxy
         self.url: str = url or "https://claude.ai"
-        self.session = aiohttp.ClientSession()
-
-    def __del__(self):
-        asyncio.run(self.session.close())
+        self.timeout: int = timeout
 
     async def ask_with_stream(self, organization_uuid, conversation_uuid, message):
         """向 Claude AI 发送消息"""
@@ -21,12 +18,25 @@ class ClaudeWeb:
         headers = self._get_headers()
         payload = self._get_payload(organization_uuid, conversation_uuid, message)
         try:
-            async with self.session.post(request_url, headers=headers, json=payload, proxy=self.proxy) as resp:
-                async for line in resp.content:
-                    line = line.decode('utf-8').strip()
-                    if line.startswith('data:'):
-                        data_json = json.loads(line[5:])
+            async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=self.timeout)) as session:
+                async with session.post(request_url, headers=headers, json=payload, proxy=self.proxy) as resp:
+                    completion_text: str = ''
+                    async for line in resp.content:
+                        try:
+                            line = line.decode('utf-8').strip()
+                            if not line.startswith("data:"):
+                                continue
+                            line = line[len("data:"):]
+                            if not line:
+                                continue
+                            data_json = json.loads(line)
+                        except json.JSONDecodeError:
+                            raise Exception(f"JSON解码错误: {line}") from None
+                        completion_text = data_json['completion']
+                        if data_json.get('stop_reason') == 'stop_sequence':
+                            break
                         yield data_json['completion']
+            logger.debug(f"[Claude] 完整回复内容 - {completion_text}")
         except Exception as e:
             logger.error(f"An error occurred in ask_with_stream: {e}")
             raise
@@ -38,11 +48,12 @@ class ClaudeWeb:
         session_uuid = str(uuid.uuid4())
         payload = {"uuid": session_uuid, "name": name}
         try:
-            async with self.session.post(request_url, headers=headers, json=payload, proxy=self.proxy) as resp:
-                data = await resp.text()
-                data_json = json.loads(data)
-                print(data_json)
-                return session_uuid
+            async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=self.timeout)) as session:
+                async with session.post(request_url, headers=headers, json=payload, proxy=self.proxy) as resp:
+                    data = await resp.text()
+                    data_json = json.loads(data)
+                    logger.debug(data_json)
+            return session_uuid
         except Exception as e:
             logger.error(f"An error occurred in create_session: {e}")
             raise
@@ -52,10 +63,11 @@ class ClaudeWeb:
         request_url = f'{self.url}/api/organizations/{organization_uuid}/chat_conversations/{conversation_uuid}'
         headers = self._get_headers()
         try:
-            async with self.session.get(request_url, headers=headers, proxy=self.proxy) as resp:
-                data = await resp.text()
-                data_json = json.loads(data)
-                return data_json
+            async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=self.timeout)) as session:
+                async with session.get(request_url, headers=headers, proxy=self.proxy) as resp:
+                    data = await resp.text()
+                    data_json = json.loads(data)
+                    return data_json
         except Exception as e:
             logger.error(f"An error occurred in get_conversation: {e}")
             raise
@@ -65,11 +77,12 @@ class ClaudeWeb:
         request_url = f'{self.url}/api/organizations/{organization_uuid}/chat_conversations'
         headers = self._get_headers()
         try:
-            async with self.session.get(request_url, headers=headers, proxy=self.proxy) as resp:
-                data = await resp.text()
-                data_json = json.loads(data)
-                session_uuids = [session['uuid'] for session in data_json]
-                return session_uuids
+            async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=self.timeout)) as session:
+                async with session.get(request_url, headers=headers, proxy=self.proxy) as resp:
+                    data = await resp.text()
+                    data_json = json.loads(data)
+                    session_uuids = [session['uuid'] for session in data_json]
+                    return session_uuids
         except Exception as e:
             logger.error(f"An error occurred in get_session: {e}")
             raise
@@ -79,11 +92,12 @@ class ClaudeWeb:
         request_url = f'{self.url}/api/organizations'
         headers = self._get_headers()
         try:
-            async with self.session.get(request_url, headers=headers, proxy=self.proxy) as resp:
-                data = await resp.text()
-                data_json = json.loads(data)
-                organization_uuid = data_json[0]["uuid"]
-                return organization_uuid
+            async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=self.timeout)) as session:
+                async with session.get(request_url, headers=headers, proxy=self.proxy) as resp:
+                    data = await resp.text()
+                    data_json = json.loads(data)
+                    organization_uuid = data_json[0]["uuid"]
+                    return organization_uuid
         except Exception as e:
             logger.error(f"An error occurred in get_uuid: {e}")
             raise
@@ -111,22 +125,23 @@ class ClaudeWeb:
 
     async def main(self):
         """测试函数"""
+
         # 获得用户的uuid
         organization_uuid = await self.get_uuid()
 
         # 获得用户的所有会话
-        await self.get_session(organization_uuid)
+        # await self.get_session(organization_uuid)
 
         # 创建一个会话
-        conversation_uuid = self.create_session(organization_uuid)
+        conversation_uuid = await self.create_session(organization_uuid)
 
         # 获得某个会话的所有消息
-        await self.get_conversation(organization_uuid, conversation_uuid)
+        # await self.get_conversation(organization_uuid, conversation_uuid)
 
         # 向Claude AI发送消息
-        # message = "hello"
-        # async for completion in self.ask_with_stream(organization_uuid, conversation_uuid, message):
-        #     logger.debug(completion)
+        message = "hello"
+        async for completion in self.ask_with_stream(organization_uuid, conversation_uuid, message):
+            logger.debug(completion)
 
 
 if __name__ == "__main__":
